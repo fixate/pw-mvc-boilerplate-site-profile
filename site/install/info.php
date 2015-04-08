e17ef904c8246e432fcb30cdbd051d43175cb2b5
dewald-laubscher <laubscher.dewald@gmail.com>
2015-04-08 17:35:27 +0200

    add initial commit from test repo

---
+++ site/install/info.php
@@ -0,0 +1,6 @@
+<?php if(!defined("PROCESSWIRE_INSTALL")) die();
+$info = array(
+	'title' => "Fixate PW-MVC Boilerplate", 
+	'summary' => "An empty site profile for fixate processwire boilerplate. ", 
+	'screenshot' => ""
+	);
